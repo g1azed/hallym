87e0b1f8c9350628cf882cc348795624ddf3f012
SUZY <victoria1230120@gmail.com>
2025-01-10 17:44:40 +0900

    메인 섹션-2 호버 인터렉션 추가

---
+++ js/more-view.js
@@ -0,0 +1,8 @@
+// more view Button
+document.getElementById('moreViewBtn').addEventListener('click', function() {
+    const imgWrap = document.querySelector('.mainIndexRight_imgWrap');
+    const newImgWrap = document.querySelector('.mainIndexRight_imgWrap');
+    newImg.src = './asset/MainIndex/stickySection/sticky_group.png';
+    imgWrap.appendChild(newImgWrap);
+
+});
