7d6f27c5e0f0569f70b86ff94e9ffc7b17d2da5a
SUZY <victoria1230120@gmail.com>
2025-01-10 15:50:14 +0900

    asset 정리 및 src 다시 연결

--- js/header_main.js
+++ js/header_main.js
@@ -1,22 +1,22 @@
 // 스크롤 시 header fade-in
 $(document).on('scroll', function(){
     if($(window).width() >768){
-        if($(window).scrollTop() > 1980){
+        if($(window).scrollTop() >= 1838){
             $("#header").removeClass("deactive");
             $("#header").addClass("active");
 
-            $('.head_logo_p').attr('src', '../asset/hanrimLOGO_color.svg')
-            $('.langIcon').attr('src', '../asset/KoreanIcon_b.svg')
-            $('.searchIcon').attr('src', '../asset/SearchIcon_b.svg')
-            $('.menuIcon').attr('src', '../asset/headerBarIcon_b.svg')
+            $('.head_logo_p').attr('src', '../asset/header/hanrimLOGO_color.svg')
+            $('.langIcon').attr('src', '../asset/Icon/KoreanIcon_b.svg')
+            $('.searchIcon').attr('src', '../asset/Icon/SearchIcon_b.svg')
+            $('.menuIcon').attr('src', '../asset/Icon/headerBarIcon_b.svg')
         }else{
             $("#header").removeClass("active");
             $("#header").addClass("deactive");
 
-            $('.head_logo_p').attr('src', '../asset/hanrimLOGO.svg')
-            $('.langIcon').attr('src', '../asset/KoreanIcon.svg')
-            $('.searchIcon').attr('src', '../asset/SearchIcon.svg')
-            $('.menuIcon').attr('src', '../asset/headerBarIcon.svg')
+            $('.head_logo_p').attr('src', '../asset/header/hanrimLOGO.svg')
+            $('.langIcon').attr('src', '../asset/Icon/KoreanIcon.svg')
+            $('.searchIcon').attr('src', '../asset/Icon/SearchIcon.svg')
+            $('.menuIcon').attr('src', '../asset/Icon/headerBarIcon.svg')
         }
     }
 })
