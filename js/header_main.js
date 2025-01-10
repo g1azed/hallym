df34b346830df831a64fd6e40b3fc6c3991d30a5
SUZY <victoria1230120@gmail.com>
2025-01-10 16:12:16 +0900

    이미지 경로 테스트

--- js/header_main.js
+++ js/header_main.js
@@ -5,7 +5,7 @@ $(document).on('scroll', function(){
             $("#header").removeClass("deactive");
             $("#header").addClass("active");
 
-            $('.head_logo_p').attr('src', '../asset/header/hanrimLOGO_color.svg')
+            $('.head_logo_p').attr('src', 'asset/header/hanrimLOGO_color.svg')
             $('.langIcon').attr('src', '../asset/Icon/KoreanIcon_b.svg')
             $('.searchIcon').attr('src', '../asset/Icon/SearchIcon_b.svg')
             $('.menuIcon').attr('src', '../asset/Icon/headerBarIcon_b.svg')
@@ -13,7 +13,7 @@ $(document).on('scroll', function(){
             $("#header").removeClass("active");
             $("#header").addClass("deactive");
 
-            $('.head_logo_p').attr('src', '../asset/header/hanrimLOGO.svg')
+            $('.head_logo_p').attr('src', 'asset/header/hanrimLOGO.svg')
             $('.langIcon').attr('src', '../asset/Icon/KoreanIcon.svg')
             $('.searchIcon').attr('src', '../asset/Icon/SearchIcon.svg')
             $('.menuIcon').attr('src', '../asset/Icon/headerBarIcon.svg')
