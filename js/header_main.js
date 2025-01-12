e634ae495557478660aff13f26b810cb7b90d55f
SUZY <victoria1230120@gmail.com>
2025-01-12 19:03:09 +0900

    메인 인터렉션 수정

--- js/header_main.js
+++ js/header_main.js
@@ -1,29 +1,36 @@
-// 스크롤 시 header fade-in
-$(document).on('scroll', function(){
-    if($(window).width() >768){
-        if($(window).scrollTop() >= 4818){
-            $("#header").removeClass("deactive");
-            $("#header").addClass("active");
 
-            $("#header").css('opacity','1')
-            // $('.head_logo_p').attr('src', 'asset/header/hanrimLOGO_color.svg')
-            $('.head_logo_p').attr('src', 'https://g1azed.github.io/hallym/asset/header/hanrimLOGO_color.svg')
-            $('.langIcon').attr('src', 'https://g1azed.github.io/hallym/asset/Icon/KoreanIcon_b.svg')
-            $('.searchIcon').attr('src', 'https://g1azed.github.io/hallym/asset/Icon/SearchIcon_b.svg')
-            $('.menuIcon').attr('src', 'https://g1azed.github.io/hallym/asset/Icon/headerBarIcon_b.svg')
-        }else if($(window).scrollTop() <= 3780 && $(window).scrollTop() >= 0){
-            $("#header").css('opacity','0')
-        }else{
-            $("#header").css('opacity','1')
-            $("#header").removeClass("active");
-            $("#header").addClass("deactive");
 
-            $('.head_logo_p').attr('src', 'https://g1azed.github.io/hallym/asset/header/hanrimLOGO.svg')
-            $('.langIcon').attr('src', 'https://g1azed.github.io/hallym/asset/Icon/KoreanIcon.svg')
-            $('.searchIcon').attr('src', 'https://g1azed.github.io/hallym/asset/Icon/SearchIcon.svg')
-            $('.menuIcon').attr('src', 'https://g1azed.github.io/hallym/asset/Icon/headerBarIcon.svg')
-        }
-    }
-})
 
-$Text_hallym.css('transform', 'translateX(0)');
+
+$(document).ready(function () {
+    $("#header").css('opacity','0')
+    // 스크롤 시 header fade-in
+    $(document).on('scroll', function(){
+        $("#header").css('opacity','0')
+            if($(window).width() >768){
+                if($(window).scrollTop() >= 4818){
+                $("#header").removeClass("deactive");
+                $("#header").addClass("active");
+
+                $("#header").css('opacity','1')
+                // $('.head_logo_p').attr('src', 'asset/header/hanrimLOGO_color.svg')
+                $('.head_logo_p').attr('src', 'https://g1azed.github.io/hallym/asset/header/hanrimLOGO_color.svg')
+                $('.langIcon').attr('src', 'https://g1azed.github.io/hallym/asset/Icon/KoreanIcon_b.svg')
+                $('.searchIcon').attr('src', 'https://g1azed.github.io/hallym/asset/Icon/SearchIcon_b.svg')
+                $('.menuIcon').attr('src', 'https://g1azed.github.io/hallym/asset/Icon/headerBarIcon_b.svg')
+            }else if($(window).scrollTop() <= 3780 && $(window).scrollTop() > -10){
+                $("#header").css('opacity','0')
+            }else{
+                $("#header").css('opacity','1')
+                $("#header").removeClass("active");
+                $("#header").addClass("deactive");
+
+                $('.head_logo_p').attr('src', 'https://g1azed.github.io/hallym/asset/header/hanrimLOGO.svg')
+                $('.langIcon').attr('src', 'https://g1azed.github.io/hallym/asset/Icon/KoreanIcon.svg')
+                $('.searchIcon').attr('src', 'https://g1azed.github.io/hallym/asset/Icon/SearchIcon.svg')
+                $('.menuIcon').attr('src', 'https://g1azed.github.io/hallym/asset/Icon/headerBarIcon.svg')
+            }
+        }   
+    })
+
+});
--- js/horizon-text.js
+++ js/horizon-text.js
@@ -4,26 +4,44 @@ $(document).ready(function () {
     const $Text_allnew = $(".allnew");
     const $Text_hallym = $(".hallym");
     const $Indicator = $(".indicator-bar");
+
+    // 애니메이션 끝난 후 이벤트
+    //333 
+    $(".allnew").on("animationend", function() {
+        $(this).css({
+            "animation-name": "none", // 애니메이션 이름 제거
+            "opacity" : "1",
+        });
+    }); 
+
     
     $(window).on('scroll', function () {
         // 스크롤 위치 가져오기
         const scrollTop = $(this).scrollTop();
+    
 
-        // if (scrollTop >160) {
-        //     $Text_allnew.css('transform', 'translateY(0)');
-        //     $Text_allnew.css('clip-path', '  inset(-100% 0% -100% 0%)');
-        // } else if(scrollTop <= 160 && scrollTop == 0){
-        //     $Text_allnew.css('transform', 'translateY(-100px)');
-        //     $Text_allnew.css('clip-path', ' inset(50% 0% 50% 0%)');
-        // }
+        
+        if (scrollTop < 800 && scrollTop >= 0) {
+            $Text_allnew.css('transform', 'translateX(0)');
+            $Text_allnew.css('clip-path', '  inset(-100% 0% -100% 0%)');
+        } else if(scrollTop >= 800){
+            $Text_allnew.css('transform', 'translateX(-200px)');
+            $Text_allnew.css('clip-path', ' inset(-100% 0% -100% 100%)');
+        }
 
-        // 배경색 변경: 스크롤 위치에 따라 색상이 점진적으로 변함
-        if (scrollTop >1350) {
+
+        
+
+
+        if (scrollTop > 833) {
             $Text_hallym.css('transform', 'translateX(0)');
             $Text_hallym.css('clip-path', '  inset(-100% 0% -100% 0%)');
-        } else if(scrollTop <= 1800 && scrollTop == 0){
+        } else if(scrollTop <= 833 && scrollTop == 0){
             $Text_hallym.css('transform', 'translateX(200px)');
             $Text_hallym.css('clip-path', ' inset(-100% 100% -100% 0%)');
+        }else if(scrollTop >= 2930){
+            $Text_hallym.css('transform', 'translateY(200px)');
+            // $Text_hallym.css('clip-path', ' inset(0% 100% 0% 0%)');
         }
 
         if(scrollTop >= 2930){
@@ -32,6 +50,15 @@ $(document).ready(function () {
             $Indicator.css("visibility", 'visible')
         }
 
+
+        3780
+        if(scrollTop > 5271){
+            $(".bg_video").empty()
+        }else{
+            $(".bg_video").append(` <video autoplay loop muted>
+                    <source src="./asset/main_video.mp4" /> 
+                </video>`)
+        }
 
 
     });
