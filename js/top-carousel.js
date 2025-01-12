787c3a17205a91112f0b3c0f1ccfcf5bc37891a7
SUZY <victoria1230120@gmail.com>
2025-01-12 15:20:21 +0900

    세부조정

--- js/top-carousel.js
+++ js/top-carousel.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
     //     $('.slick-slide').css('display', 'flex'); // 초기화 시 display를 flex로 설정
     // });
 
-// 슬라이드 변경 시 너비 조정
-$('.carouselTop').on('afterChange', function (event, slick, currentSlide) {
-    $('.carouselTop .slick-slide .top_CellText').css('display', 'none'); 
-    $('.carouselTop .slick-center .top_CellText').css('display', 'flex'); 
+    // 슬라이드 변경 시 너비 조정
+    $('.carouselTop').on('afterChange', function (event, slick, currentSlide) {
+        $('.carouselTop .slick-slide .top_CellText').css('display', 'none'); 
+        $('.carouselTop .slick-center .top_CellText').css('display', 'flex'); 
 
-});
+    });
 
     $('.carouselTop').slick({
         lazyLoad: 'ondemand',
@@ -17,25 +17,27 @@ $('.carouselTop').on('afterChange', function (event, slick, currentSlide) {
         nextArrow : $('.top_nextArrow'), 
         autoplay: true,
         autoplaySpeed: 2000,
-            infinite: true,
-            slidesToShow: 3, // 3개 슬라이드가 보이도록 설정
-            slidesToScroll: 1,
-            centerMode: true, // 중앙 정렬
-            variableWidth: true, // 슬라이드 너비를 유동적으로 설정
-            draggable: false,
-            
-            responsive: [
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 1,
-                        variableWidth: false,
-                    }
+        infinite: true,
+        slidesToShow: 3, // 3개 슬라이드가 보이도록 설정
+        slidesToScroll: 1,
+        centerMode: true, // 중앙 정렬
+        variableWidth: true, // 슬라이드 너비를 유동적으로 설정
+        draggable: false,
+        
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    variableWidth: false,
                 }
-            ]
+            }
+        ],
+    });
 
-        });
+        
 
 });
+        
 
 
